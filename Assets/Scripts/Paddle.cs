2d0aa78e0ac7c36a7efcb56a5cfbdc0243748f13
ilpeppino <ilpeppino@gmail.com>
2019-11-21 23:18:02 +0100

    class 54 - clamp not working

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,25 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour { 
+public class Paddle : MonoBehaviour {
 
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // transform.position = new Vector2(8f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnitsInputs = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
 
         // Vector 2 defines the position of the object
-        // Vector2 paddlePos = new Vector2(mousePosInUnitsInputs, transform.position.y);
 
-        Vector2 paddlePos = new Vector2(Input.mousePosition.x, transform.position.y);
+        
+
+        Vector2 paddlePos = new Vector2();
+
+
+        if (mousePosInUnits < minX)
+        {
+            mousePosInUnits = minX;
+        }
+        else if (mousePosInUnits > maxX)
+        {
+            mousePosInUnits = maxX;
+        }
+
+
+        paddlePos.x = mousePosInUnits;
+        // paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.y = transform.position.y;
+
+        Debug.Log("BEFORE --- Mouse " + mousePosInUnits + " PX " + paddlePos.x);
+
+        
+
+        /* 
+        if (paddlePos.x < minX)
+        {
+            paddlePos.x = minX;
+        }
+        else if (paddlePos.x > maxX)
+        {
+            paddlePos.x = maxX;
+        } 
+
+        
+
+        Debug.Log("AFTER --- Mouse " + mousePosInUnits + " PX " + paddlePos.x);
+        */
+
 
         // transform is connected to the transform settings of the object in Unity, and tells to move to the specified position 
         transform.position = paddlePos;
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,17 +7,17 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadNextScene()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        // int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
     public void LoadStartScene()
     {
-        SceneManager.LoadScene(0);
+        // SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        // Application.Quit();
     }
 }
