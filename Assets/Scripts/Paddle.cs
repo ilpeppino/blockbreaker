13ed1191c22331e3149259af8bf369a03bfddd80
ilpeppino <ilpeppino@gmail.com>
2019-11-21 19:58:20 +0100

    First commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour { 
+
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnitsInputs = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        // Vector 2 defines the position of the object
+        // Vector2 paddlePos = new Vector2(mousePosInUnitsInputs, transform.position.y);
+
+        Vector2 paddlePos = new Vector2(Input.mousePosition.x, transform.position.y);
+
+        // transform is connected to the transform settings of the object in Unity, and tells to move to the specified position 
+        transform.position = paddlePos;
+    }
+}
