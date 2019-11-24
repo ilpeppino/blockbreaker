bf47498766716e5434e623a3e3d826a89e55d89d
ilpeppino <ilpeppino@gmail.com>
2019-11-24 21:48:07 +0100

    class 58 - hit and destroy block

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+
+}
