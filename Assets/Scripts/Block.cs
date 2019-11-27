a4ebcbf66c82f1844e1b815e5637fc138faf4164
ilpeppino <ilpeppino@gmail.com>
2019-11-27 18:28:21 +0100

    class 65

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,19 +5,26 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
-
     Level level;
- 
+
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.current.transform.position);
         Destroy(gameObject);
-
+        level.BlockDestroyed();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // At each instance of Block, this Start will be called and add one block through the CountBreakableBlocks function
         level = FindObjectOfType<Level>();
         level.CountBreakableBlocks();
     }
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,9 +6,25 @@ public class Level : MonoBehaviour
 
 {
     [SerializeField] int breakableBlocks;
+    SceneLoader sceneLoader;
 
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();   
     }
 }
